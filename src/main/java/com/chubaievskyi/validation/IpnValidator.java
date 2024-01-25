4e677131363172e95f7915c640b62defa785ccb0
Pavlo Chubaievskyi <chubaievskyi@gmail.com>
2024-01-25 12:01:41 +0200

    Change ipn validation

--- src/main/java/com/chubaievskyi/validation/IpnValidator.java
+++ src/main/java/com/chubaievskyi/validation/IpnValidator.java
@@ -3,12 +3,19 @@ package com.chubaievskyi.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class IpnValidator implements ConstraintValidator<IpnValidation, String> {
 
     public final int[] coefficient = {-1, 5, 7, 9, 4, 6, 10, 5, 7};
 
     @Override
     public boolean isValid(String ipn, ConstraintValidatorContext context) {
+
+        if (ipn == null || !ipn.matches("\\d{10}")) {
+            return false;
+        }
 
         int sum = 0;
         for (int i = 0; i < ipn.length() - 1; i++) {
