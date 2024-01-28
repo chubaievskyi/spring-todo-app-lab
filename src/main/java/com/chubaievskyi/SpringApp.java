defbb993a8a72ee0f99637f16b694fddc62e0a98
Pavlo Chubaievskyi <chubaievskyi@gmail.com>
2024-01-28 23:24:57 +0200

    The basic authorization window has been removed.

--- src/main/java/com/chubaievskyi/SpringApp.java
+++ src/main/java/com/chubaievskyi/SpringApp.java
@@ -8,12 +8,5 @@ public class SpringApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
-
-        // TODO: 27.01.2024  Created by: @chubaievskyi - перехопити загальний ексепшн
-        // TODO: 27.01.2024  Created by: @chubaievskyi - додати реєстрацію
-        // TODO: 28.01.2024  Created by: @chubaievskyi - додати апдейтюзердто для оновлення користувачами пароля
-        // TODO: 28.01.2024  Created by: @chubaievskyi - додати ендпойнт для оновлення пароля
-        // TODO: 28.01.2024  Created by: @chubaievskyi - прибрати httpBasic вікно
-        // TODO: 28.01.2024  Created by: @chubaievskyi - 403 статус (дто помилки)
     }
 }
--- src/main/java/com/chubaievskyi/configuration/SecurityConfiguration.java
+++ src/main/java/com/chubaievskyi/configuration/SecurityConfiguration.java
@@ -3,11 +3,13 @@ package com.chubaievskyi.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +29,12 @@ public class SecurityConfiguration {
                                 .requestMatchers(HttpMethod.DELETE, "/users/**").hasAnyAuthority(ROLE_ADMIN)
                                 .requestMatchers("/users/**").hasAnyAuthority(ROLE_ADMIN, ROLE_USER)
                                 .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN))
+                )
+                .httpBasic(Customizer.withDefaults()
+                );
         return http.build();
     }
 }
