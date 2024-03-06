460b916c05215fab1c1934944cf44e28e19b11dc
Pavlo Chubaievskyi <109543969+chubaievskyi@users.noreply.github.com>
2024-03-06 22:15:01 +0200

    Update SpringApp.java

--- src/main/java/com/chubaievskyi/SpringApp.java
+++ src/main/java/com/chubaievskyi/SpringApp.java
@@ -8,11 +8,5 @@ public class SpringApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
-
-        // TODO: 27.01.2024  Created by: @chubaievskyi - перехопити загальний ексепшн
-        // TODO: 26.02.2024  Created by: @chubaievskyi - інтернаціоналізація, локалізація
-        // TODO: 06.03.2024  Created by: @chubaievskyi - користувача не знайдено з ІД!!! (e.get message в глобалексепшнхендлері)
-        // TODO: 06.03.2024  Created by: @chubaievskyi - право перевідкривати таску тільки у адміна
-        // TODO: 06.03.2024  Created by: @chubaievskyi - перевірити роботу всіх ендпоінтів та всіх змін статусів
     }
 }
