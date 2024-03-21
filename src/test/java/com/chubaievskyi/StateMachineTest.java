831175d1364fff17acd934c4a13b396189af8fb9
Pavlo Chubaievskyi <chubaievskyi@gmail.com>
2024-03-21 22:18:25 +0200

    Tried to add some tests

--- src/test/java/com/chubaievskyi/StateMachineTest.java
+++ src/test/java/com/chubaievskyi/StateMachineTest.java
@@ -23,20 +23,93 @@ class StateMachineTest {
     @Autowired
     private StateMachine<Status, Event> stateMachine;
 
-    @Test
-    void testInitial() throws Exception {
+
+    void resetStateMachine(Status status) {
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions(accessor -> accessor.resetStateMachine(new DefaultStateMachineContext<>(status,
+                        null, null, null)));
+    }
+
+    void statusChangeTest(Status currentStatus, Event event, Status expectedStatus, int count) throws Exception {
+
+        resetStateMachine(currentStatus);
+
         StateMachineTestPlan<Status, Event> plan =
                 StateMachineTestPlanBuilder.<Status, Event>builder()
                         .stateMachine(stateMachine)
                         .step()
-                        .expectState(Status.NEW)
+                        .expectState(currentStatus)
+                        .and()
+                        .step()
+                        .sendEvent(event)
+                        .expectStates(expectedStatus)
+                        .expectStateChanged(count)
                         .and()
                         .build();
         plan.test();
     }
 
     @Test
-    void testNewStatusValidOptions() throws Exception {
+    void testValidStatusOption() throws Exception {
+
+        statusChangeTest(Status.NEW, Event.WORK_IN_PROGRESS, Status.WORK_IN_PROGRESS, 1);
+        statusChangeTest(Status.NEW, Event.POSTPONED, Status.POSTPONED, 1);
+        statusChangeTest(Status.NEW, Event.CANCELLED, Status.CANCELLED, 1);
+
+        statusChangeTest(Status.WORK_IN_PROGRESS, Event.POSTPONED, Status.POSTPONED, 1);
+        statusChangeTest(Status.WORK_IN_PROGRESS, Event.NOTIFIED, Status.NOTIFIED, 1);
+        statusChangeTest(Status.WORK_IN_PROGRESS, Event.SIGNED, Status.SIGNED, 1);
+        statusChangeTest(Status.WORK_IN_PROGRESS, Event.CANCELLED, Status.CANCELLED, 1);
+
+        statusChangeTest(Status.POSTPONED, Event.WORK_IN_PROGRESS, Status.WORK_IN_PROGRESS, 1);
+        statusChangeTest(Status.POSTPONED, Event.CANCELLED, Status.CANCELLED, 1);
+
+        statusChangeTest(Status.NOTIFIED, Event.SIGNED, Status.SIGNED, 1);
+        statusChangeTest(Status.NOTIFIED, Event.DONE, Status.DONE, 1);
+        statusChangeTest(Status.NOTIFIED, Event.CANCELLED, Status.CANCELLED, 1);
+
+        statusChangeTest(Status.SIGNED, Event.DONE, Status.DONE, 1);
+        statusChangeTest(Status.SIGNED, Event.CANCELLED, Status.CANCELLED, 1);
+
+    }
+
+    @Test
+    void testInvalidStatusOption() throws Exception {
+
+        statusChangeTest(Status.NEW, Event.NOTIFIED, Status.NEW, 0);
+        statusChangeTest(Status.NEW, Event.SIGNED, Status.NEW, 0);
+        statusChangeTest(Status.NEW, Event.DONE, Status.NEW, 0);
+
+        statusChangeTest(Status.WORK_IN_PROGRESS, Event.DONE, Status.WORK_IN_PROGRESS, 0);
+
+        statusChangeTest(Status.POSTPONED, Event.NOTIFIED, Status.POSTPONED, 0);
+        statusChangeTest(Status.POSTPONED, Event.SIGNED, Status.POSTPONED, 0);
+        statusChangeTest(Status.POSTPONED, Event.DONE, Status.POSTPONED, 0);
+
+        statusChangeTest(Status.NOTIFIED, Event.WORK_IN_PROGRESS, Status.NOTIFIED, 0);
+        statusChangeTest(Status.NOTIFIED, Event.POSTPONED, Status.NOTIFIED, 0);
+
+        statusChangeTest(Status.SIGNED, Event.WORK_IN_PROGRESS, Status.SIGNED, 0);
+        statusChangeTest(Status.SIGNED, Event.POSTPONED, Status.SIGNED, 0);
+        statusChangeTest(Status.SIGNED, Event.NOTIFIED, Status.SIGNED, 0);
+
+        statusChangeTest(Status.DONE, Event.WORK_IN_PROGRESS, Status.DONE, 0);
+        statusChangeTest(Status.DONE, Event.POSTPONED, Status.DONE, 0);
+        statusChangeTest(Status.DONE, Event.NOTIFIED, Status.DONE, 0);
+        statusChangeTest(Status.DONE, Event.SIGNED, Status.DONE, 0);
+        statusChangeTest(Status.DONE, Event.CANCELLED, Status.DONE, 0);
+
+        statusChangeTest(Status.CANCELLED, Event.WORK_IN_PROGRESS, Status.CANCELLED, 0);
+        statusChangeTest(Status.CANCELLED, Event.POSTPONED, Status.CANCELLED, 0);
+        statusChangeTest(Status.CANCELLED, Event.NOTIFIED, Status.CANCELLED, 0);
+        statusChangeTest(Status.CANCELLED, Event.SIGNED, Status.CANCELLED, 0);
+        statusChangeTest(Status.CANCELLED, Event.DONE, Status.CANCELLED, 0);
+
+
+    }
+
+    @Test
+    void happyPassChangeStatusTest() throws Exception {
         StateMachineTestPlan<Status, Event> plan =
                 StateMachineTestPlanBuilder.<Status, Event>builder()
                         .stateMachine(stateMachine)
@@ -54,102 +127,27 @@ class StateMachineTest {
                         .expectStateChanged(1)
                         .and()
                         .step()
-                        .sendEvent(Event.CANCELLED)
-                        .expectState(Status.CANCELLED)
+                        .sendEvent(Event.WORK_IN_PROGRESS)
+                        .expectState(Status.WORK_IN_PROGRESS)
                         .expectStateChanged(1)
-                        .and()
-                        .build();
-        plan.test();
-    }
-
-    @Test
-    void testNewStatusInvalidOptions() throws Exception {
-        StateMachineTestPlan<Status, Event> plan =
-                StateMachineTestPlanBuilder.<Status, Event>builder()
-                        .stateMachine(stateMachine)
-                        .step()
-                        .expectState(Status.NEW)
                         .and()
                         .step()
                         .sendEvent(Event.NOTIFIED)
-                        .expectStates(Status.NEW)
-                        .expectStateChanged(0)
+                        .expectState(Status.NOTIFIED)
+                        .expectStateChanged(1)
                         .and()
                         .step()
                         .sendEvent(Event.SIGNED)
-                        .expectState(Status.NEW)
-                        .expectStateChanged(0)
+                        .expectState(Status.SIGNED)
+                        .expectStateChanged(1)
                         .and()
                         .step()
                         .sendEvent(Event.DONE)
-                        .expectState(Status.NEW)
-                        .expectStateChanged(0)
-                        .and()
-                        .build();
-        plan.test();
-    }
-
-    @Test
-    void testWorkInProgressStatusValidOptions() throws Exception {
-
-        resetStateMachine(Status.WORK_IN_PROGRESS);
-
-        StateMachineTestPlan<Status, Event> plan =
-                StateMachineTestPlanBuilder.<Status, Event>builder()
-                        .stateMachine(stateMachine)
-                        .step()
-                        .expectState(Status.WORK_IN_PROGRESS)
-                        .and()
-                        .step()
-                        .sendEvent(Event.POSTPONED)
-                        .expectState(Status.POSTPONED)
-                        .expectStateChanged(1)
-                        .and()
-//                        .step()
-//                        .sendEvent(Event.NOTIFIED)
-//                        .expectState(Status.NOTIFIED)
-//                        .expectStateChanged(1)
-//                        .and()
-//                        .step()
-//                        .sendEvent(Event.SIGNED)
-//                        .expectStates(Status.SIGNED)
-//                        .expectStateChanged(1)
-//                        .and()
-                        .step()
-                        .sendEvent(Event.CANCELLED)
-                        .expectStates(Status.CANCELLED)
+                        .expectState(Status.DONE)
                         .expectStateChanged(1)
                         .and()
                         .build();
         plan.test();
-    }
-
-    @Test
-    void testWorkInProgressStatusInvalidOptions() throws Exception {
-
-        resetStateMachine(Status.WORK_IN_PROGRESS);
-
-        StateMachineTestPlan<Status, Event> plan =
-                StateMachineTestPlanBuilder.<Status, Event>builder()
-                        .stateMachine(stateMachine)
-                        .step()
-                        .expectState(Status.WORK_IN_PROGRESS)
-                        .and()
-                        .step()
-                        .sendEvent(Event.DONE)
-                        .expectStates(Status.WORK_IN_PROGRESS)
-                        .expectStateChanged(0)
-                        .and()
-                        .build();
-        plan.test();
-    }
-
-    void resetStateMachine(Status status) {
-        stateMachine.getStateMachineAccessor()
-                .doWithAllRegions(accessor -> {
-                    accessor.resetStateMachine(new DefaultStateMachineContext<>(status,
-                            null, null, null));
-                });
     }
 
 }
